5464a39a92e5bc1aa0810f46edb6f53ad602603a
Pat Wilson <zerostride@gmail.com>
2018-11-01 14:08:40 -0500

    Don't nuke the CWD...

--- lib/headdesk/cli.rb
+++ lib/headdesk/cli.rb
@@ -10,14 +10,17 @@ module Headdesk
   class CLI < Thor
     desc 'unpack PATH', 'Unpack an APK or IPA'
     method_options destination: :string
-    def unpack(path, destination = Dir.pwd)
+    def unpack(path, destination = nil)
       unless File.exist?(path)
         STDERR.puts "Could not find: #{path}"
         CLI.command_help(Thor::Base.shell.new, 'unpack')
         return 1
       end
 
-      stdout, stderr, exit_code = Headdesk::ApkTool.cmd(path, 'd', '--force', '--output', destination, path)
+      args = ['d', '--force']
+      args.concat(['--output', destination]) unless destination.nil?
+
+      stdout, stderr, exit_code = Headdesk::ApkTool.cmd(*args, path)
       unless exit_code.to_i.zero?
         STDERR.puts stderr
         return 1
