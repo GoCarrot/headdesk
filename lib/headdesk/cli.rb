a61dc8db10df091c9283c4309820ece026fc1117
Pat Wilson <zerostride@gmail.com>
2018-11-01 14:28:56 -0500

    Can now unpack to cwd

--- lib/headdesk/cli.rb
+++ lib/headdesk/cli.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'tmpdir'
 require 'thor'
 require 'headdesk'
 
@@ -8,25 +9,38 @@ module Headdesk
   # headdesk CLI
   #
   class CLI < Thor
-    desc 'unpack PATH', 'Unpack an APK or IPA'
+    desc 'unpack PATH [DESTINATION]', 'Unpack an APK or IPA to <DESTINATION> or the current directory'
     method_options destination: :string
     def unpack(path, destination = nil)
+      # Make sure the input path exists
       unless File.exist?(path)
         STDERR.puts "Could not find: #{path}"
         CLI.command_help(Thor::Base.shell.new, 'unpack')
         return 1
       end
 
-      args = ['d', '--force']
-      args.concat(['--output', destination]) unless destination.nil?
-
-      stdout, stderr, exit_code = Headdesk::ApkTool.cmd(*args, path)
-      unless exit_code.to_i.zero?
-        STDERR.puts stderr
+      # Make sure destination exists, if specified
+      unless destination.nil? || Dir.exist?(destination)
+        STDERR.puts "Could not find destination path: #{destination}"
+        CLI.command_help(Thor::Base.shell.new, 'unpack')
         return 1
       end
 
-      puts stdout
+      Dir.mktmpdir do |tmp_dir|
+        args = ['d', '--force']
+
+        args.concat(['--output', tmp_dir]) if destination.nil?
+        args.concat(['--output', destination]) unless destination.nil?
+
+        stdout, stderr, exit_code = Headdesk::ApkTool.cmd(*args, path)
+        unless exit_code.to_i.zero?
+          STDERR.puts stderr
+          return exit_code.to_i
+        end
+
+        FileUtils.cp_r("#{tmp_dir}/.", Dir.pwd) if destination.nil?
+        puts stdout
+      end
     end
   end
 end
