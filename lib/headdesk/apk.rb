40a130da1b7e50c4c019d684e0df12af6b2e5bfd
Pat Wilson <zerostride@gmail.com>
2018-11-01 18:22:03 -0500

    Let's do some things with the bytecode

--- lib/headdesk/apk.rb
+++ lib/headdesk/apk.rb
@@ -3,24 +3,44 @@
 require 'nokogiri'
 require 'yaml'
 
+require 'headdesk/apk/class'
+
 module Headdesk
   #
   # Representation of an APK file unpacked by apktool
   #
   class Apk
+    attr_accessor :yaml, :sdk_info
     def initialize(path)
-      android_manifest_xml = File.join(path, 'AndroidManifest.xml').freeze
-      apktool_yml = File.join(path, 'apktool.yml').freeze
+      @path = path
+
+      android_manifest_xml = File.join(@path, 'AndroidManifest.xml').freeze
+      apktool_yml = File.join(@path, 'apktool.yml').freeze
 
       unless File.exist?(android_manifest_xml) && File.exist?(apktool_yml)
         throw ArgumentError.new('Path did not contain AndroidManifest.xml and/or apktool.yml')
       end
 
       @yaml = YAML.load_file(apktool_yml)
+      @sdk_info = @yaml['sdkInfo']
     end
 
     def analize
-      puts @yaml.inspect
+      puts @sdk_info.inspect
+
+      # If targetSdkVersion >= 26 check for 'setChannelId' in
+      #    android/support/v4/app/NotificationCompat$Builder
+      # it will only be present if the v4 support lib is 26.1+
+      if @sdk_info['targetSdkVersion'].to_i > 25
+        notification_compat_builder = find_class('android/support/v4/app/NotificationCompat$Builder')
+        notification_compat_builder.method?('setChannelId')
+      end
+
+      puts 'foo'
+    end
+
+    def find_class(decl)
+      Class.new(File.join(@path, 'smali', "#{Class.path_for(decl)}.smali"))
     end
   end
 end
---
+++ lib/headdesk/apk/class.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Headdesk
+  class Apk
+    #
+    # A Smali bytecode class
+    #
+    class Class
+      # Formats:
+      #   android/content/Context
+      #   android.content.Context
+      def self.path_for(decl)
+        File.join(*decl.split(%r{[\/\.]}))
+      end
+
+      def initialize(smali_file)
+        @smali = File.read(smali_file)
+      end
+
+      def method?(name)
+        !method(name).nil?
+      end
+
+      def method(method_name)
+        /(^\.method .* #{method_name}.*;$[\s\S]*?\.end method)/.match(@smali)
+      end
+    end
+  end
+end
