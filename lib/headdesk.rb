904da09aae92147ed425f03e4b78e2715ecc13c6
Pat Wilson <zerostride@gmail.com>
2018-11-03 21:41:46 -0700

    Fetch/parse/store Facebook Android SDK versions

--- lib/headdesk.rb
+++ lib/headdesk.rb
@@ -9,4 +9,5 @@ require 'headdesk/apk'
 # headdesk - Because we've all been there with Android.
 #
 module Headdesk
+  FACEBOOK_SDK_VERSIONS_YAML = File.expand_path(File.join(File.dirname(__FILE__), 'headdesk', 'data', 'facebook_sdk_versions.yaml'))
 end
