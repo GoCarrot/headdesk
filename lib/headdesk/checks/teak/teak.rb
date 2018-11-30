edc5f2c4c0b4da2b7134b6b9a6cb5e7ca1ff8009
Pat Wilson <zerostride@gmail.com>
2018-11-30 01:01:44 -0800

    Some Teak config checks, really should figure out how to do a top-level "Teak test" and then have dependant tests

---
+++ lib/headdesk/checks/teak/teak.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+module Headdesk
+  module Teak
+    #
+    # Check to make sure that an APK, which uses Teak, has caching enabled.
+    #
+    class Configuration
+      include Check::APK
+
+      describe 'Check Teak configuration'
+      def call
+        skip_check unless: -> { @apk.class?('io.teak.sdk.Teak') }
+
+        major, minor, revision = @apk.find_class('io.teak.sdk.Teak')
+                                     .field('SDKVersion')
+                                     .value
+                                     .to_version
+        export version: "#{major}.#{minor}.#{revision}",
+               major: major,
+               minor: minor,
+               revision: revision
+
+        # App Id
+        teak_app_id = @apk.resources
+                          .values
+                          .string
+                          .io_teak_app_id
+        # TODO: Manifest meta-data
+        describe 'Teak App Id configured'
+        fail_check if: teak_app_id.nil?
+        export teak_app_id: teak_app_id
+
+        # Api Key
+        teak_api_key = @apk.resources
+                           .values
+                           .string
+                           .io_teak_api_key
+        # TODO: Manifest meta-data
+        describe 'Teak API Key configured'
+        fail_check if: teak_api_key.nil?
+        export teak_api_key: teak_api_key
+
+        # GCM Sender Id
+        io_teak_gcm_sender_id = @apk.resources
+                                    .values
+                                    .string
+                                    .io_teak_gcm_sender_id
+        # TODO: Manifest meta-data
+        gcm_defaultSenderId ||= @apk.resources
+                                    .values
+                                    .string
+                                    .gcm_defaultSenderId
+        describe "'gcm_defaultSenderId' is specified, and different from 'io_teak_gcm_sender_id'"
+        fail_check if: gcm_defaultSenderId != io_teak_gcm_sender_id unless gcm_defaultSenderId.nil?
+
+        gcm_sender_id = io_teak_gcm_sender_id || gcm_defaultSenderId
+        describe "Either 'io_teak_gcm_sender_id' or 'gcm_defaultSenderId' configured"
+        fail_check if: gcm_sender_id.nil?
+        export gcm_sender_id: gcm_sender_id
+
+        describe 'Teak SDK version is greater than 2.0.0'
+        skip_check if: major < 2
+
+        # Firebase App Id
+        io_teak_firebase_app_id = @apk.resources
+                                      .values
+                                      .string
+                                      .io_teak_firebase_app_id
+        # TODO: Manifest meta-data
+        google_app_id = @apk.resources
+                            .values
+                            .string
+                            .google_app_id
+        describe "'google_app_id' is specified, and different from 'io_teak_firebase_app_id'"
+        fail_check if: google_app_id != io_teak_firebase_app_id unless google_app_id.nil?
+
+        firebase_app_id = google_app_id || io_teak_firebase_app_id
+        describe "Either 'io_teak_firebase_app_id' or 'google_app_id' configured"
+        fail_check if: firebase_app_id.nil?
+        export gcm_sender_id: gcm_sender_id
+      end
+    end
+  end
+end
