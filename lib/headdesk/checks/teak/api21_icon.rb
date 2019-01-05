144b57344f40d363de5e63042d8d328529d62fca
Pat Wilson <zerostride@gmail.com>
2019-01-05 12:58:39 -0800

    Better v21 icon check

--- lib/headdesk/checks/teak/api21_icon.rb
+++ lib/headdesk/checks/teak/api21_icon.rb
@@ -13,10 +13,6 @@ module Headdesk
       def call
         skip_check if: -> { apk.min_sdk 21 }
 
-        describe "APK contains a drawable for 'io_teak_small_notification_icon'"
-        fail_check if: !apk.resources.values(v: 21)
-                           .drawable.io_teak_small_notification_icon
-
         icon = apk.resources
                   .values(v: 20)
                   .drawable.io_teak_small_notification_icon
@@ -25,8 +21,11 @@ module Headdesk
                       .drawable.io_teak_small_notification_icon
         export icon: icon, icon_v21: icon_v21
 
+        describe "APK contains a drawable for 'io_teak_small_notification_icon'"
+        fail_check unless: -> { icon && icon_v21 }
+
         describe "'io_teak_small_notification_icon' for v21 is different from < v21"
-        fail_check unless: icon != icon_v21
+        fail_check unless: -> { icon != icon_v21 }
       end
     end
   end
