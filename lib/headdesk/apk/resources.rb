6191873a888d7c93bcf2f1ff25c614a87480e88b
Pat Wilson <zerostride@gmail.com>
2019-01-05 17:20:24 -0800

    Clean up the XmlCollection initialization

--- lib/headdesk/apk/resources.rb
+++ lib/headdesk/apk/resources.rb
@@ -20,22 +20,15 @@ module Headdesk
       # Collection of XML values for specific locale/api/etc
       #
       class XmlCollection
-        # :reek:NestedIterators
+        # :reek:NestedIterators and :reek:TooManyStatements
         def initialize(path, type, modifiers = {})
-          mods = [nil]
-          if modifiers.key?(:v)
-            (1..modifiers[:v].to_i).each do |api_version|
-              mods << "-v#{api_version}"
-            end
-          end
-          glob = File.join(path, 'res', "#{type}{#{mods.join(',')}}", '*.xml')
-
           @resources = {}
-          Dir.glob(glob).each do |file_name|
+          globspec = File.join(path, 'res', "#{type}{#{XmlCollection.api_versions(modifiers).join(',')}}", '*.xml')
+          Dir.glob(globspec).each do |file_name|
             xml = File.open(file_name) { |file| Nokogiri::XML(file) }
 
-            load_named_elements(xml)
-            load_item_elements(xml)
+            @resources.merge! XmlCollection.named_elements(xml)
+            @resources.merge! XmlCollection.item_elements(xml)
           end
         end
 
@@ -51,36 +44,50 @@ module Headdesk
 
         private
 
-        # :reek:FeatureEnvy
-        def load_named_elements(xml)
+        def self.api_versions(modifiers)
+          mods = [nil]
+          if modifiers.key?(:v)
+            (1..modifiers[:v].to_i).each do |api_version|
+              mods << "-v#{api_version}"
+            end
+          end
+          mods
+        end
+
+        # :reek:TooManyStatements
+        def self.named_elements(xml)
           named_elements = %i[string integer color bool]
 
+          resources = {}
           xml.xpath("//#{named_elements.join('|//')}").each do |elem|
             type = elem.name.to_s
             name = elem.attributes['name'].to_s
 
-            @resources[type] ||= {}
-            @resources[type][name] = case type
-                                     when 'bool'
-                                       elem.text == true.to_s
-                                     when 'integer'
-                                       elem.text.to_i
-                                     else
-                                       elem.text
-                                     end
+            resources[type] ||= {}
+            resources[type][name] = case type
+                                    when 'bool'
+                                      elem.text == true.to_s
+                                    when 'integer'
+                                      elem.text.to_i
+                                    else
+                                      elem.text
+                                    end
           end
+          resources
         end
 
-        # :reek:FeatureEnvy
-        def load_item_elements(xml)
+        # :reek:TooManyStatements
+        def self.item_elements(xml)
           item_elements = %i[drawable]
+          resources = {}
           xml.xpath("//item[#{item_elements.map { |elem| "contains(@type, '#{elem}')" }.join('or')}]").each do |elem|
             type = elem.attributes['type'].to_s
             name = elem.attributes['name'].to_s
 
-            @resources[type] ||= {}
-            @resources[type][name] = elem.text
+            resources[type] ||= {}
+            resources[type][name] = elem.text
           end
+          resources
         end
       end
     end
